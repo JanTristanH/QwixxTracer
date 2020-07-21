c31604452cb7ae9342c4534790d4627a83e3ec95
Tristan <jan_tristan.heisecke@nordakademie.de>
2020-07-21 09:03:07 +0200

    added show functionality to result row

--- controller.js
+++ controller.js
@@ -13,7 +13,7 @@ const lockEmoji = "🔓";
 const maxNumber = 13;
 let gameOver = false;
 
-let oLowestBox= {
+let oLowestBox = {
 	red: 0,
 	yellow: 0,
 	green: 0,
@@ -25,72 +25,85 @@ let oNumberMarkedBoxes = {
 	yellow: 0,
 	green: 0,
 	blue: 0,
-	mistakes: 0
+	mistake: 0
 };
-function markBox (source,color) {
+
+function markBox(source, color) {
 	evaluateGameState();
-	if (gameOver){
-	return;
-	}
-	
-	if (source.innerText === lockEmoji ){
-		if (oLowestBox[color] == 12){
-			source.classList.add("checked");
-			oLowestBox[color] = maxNumber;
-			oNumberMarkedBoxes[color]++; 
-			
-		} 
+	if (gameOver) {
 		return;
 	}
-	
-	if(source.innerText == 12 && oNumberMarkedBoxes[color] > 4 ){
+
+	if (source.innerText === lockEmoji) {
+		if (oLowestBox[color] == 12) {
+			source.classList.add("checked");
+			oLowestBox[color] = maxNumber;
+			oNumberMarkedBoxes[color]++;
+
+		}
+		return;
+	}
+
+	if (source.innerText == 12 && oNumberMarkedBoxes[color] > 4) {
 		source.classList.add("checked");
 		oLowestBox[color] = parseInt(source.innerText);
-		oNumberMarkedBoxes[color]++; 
+		oNumberMarkedBoxes[color]++;
 	}
-	
+
 	if (oLowestBox[color] < source.innerText && source.innerText < 12) {
 		source.classList.add("checked");
 		oLowestBox[color] = parseInt(source.innerText);
-		oNumberMarkedBoxes[color]++; 
+		oNumberMarkedBoxes[color]++;
 	}
-	
+
 };
 
 function evaluateGameState() {
-if(oNumberMarkedBoxes.mistakes > 3) {
-	gameOver = true;
-} else {
-	let closedRows = 0;
-	oLowestBox.red == maxNumber ? closedRows++ : 0;
-	oLowestBox.yellow == maxNumber ? closedRows++ : 0;
-	oLowestBox.green == maxNumber ? closedRows++ : 0;
-	oLowestBox.blue == maxNumber ? closedRows++ : 0;
-	gameOver = closedRows > 1;
-	
-}
+	if (oNumberMarkedBoxes.mistake > 3) {
+		gameOver = true;
+	} else {
+		let closedRows = 0;
+		oLowestBox.red == maxNumber ? closedRows++ : 0;
+		oLowestBox.yellow == maxNumber ? closedRows++ : 0;
+		oLowestBox.green == maxNumber ? closedRows++ : 0;
+		oLowestBox.blue == maxNumber ? closedRows++ : 0;
+		gameOver = closedRows > 1;
+
+	}
 }
 
-function markBoxMistake (source){
+function markBoxMistake(source) {
 	evaluateGameState();
-	if (gameOver){
-	return;
+	if (gameOver) {
+		return;
 	}
 	source.classList.add("checked");
-	oNumberMarkedBoxes.mistakes++; 
+	oNumberMarkedBoxes.mistake++;
 };
 
-function convertScore(n){
+function convertScore(n) {
 	let score = 0;
-	for (let i = 1 ; i <= n; i++){
+	for (let i = 1; i <= n; i++) {
 		score += i;
 	}
 	return score;
 }
-function onEvaluate(){
-let score = convertScore(oNumberMarkedBoxes.red) + convertScore(oNumberMarkedBoxes.yellow) + convertScore(oNumberMarkedBoxes.green) + convertScore(oNumberMarkedBoxes.blue);
-score -= oNumberMarkedBoxes.mistakes * 5;
-$("#result").empty();
-$("#result").append(score);
+
+function onEvaluate() {
+	fillInterimResult();
+	let score = convertScore(oNumberMarkedBoxes.red) + convertScore(oNumberMarkedBoxes.yellow) + convertScore(oNumberMarkedBoxes.green) + convertScore(oNumberMarkedBoxes.blue);
+	score -= oNumberMarkedBoxes.mistake * 5;
+	$("#result").empty();
+	$("#result").append(score);
 };
+
+function fillInterimResult() {
+	for (const [key, value] of Object.entries(oNumberMarkedBoxes)) {
+		//console.log(`${key}Result: ${value}`);
+		$(`#${key}Result`).empty();
+		$(`#${key}Result`).append(
+			key == "mistake" ? value * 5 :convertScore(value));
+	}
+};
+
 console.log("script loaded");
